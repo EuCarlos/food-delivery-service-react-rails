72dad44eeaf76ea818bbd63b6db17c36737847e3
Carlos Alve <carlosalves369m@hotmail.com>
2022-06-29 11:32:23 -0300

    :card_file_box: create ActiveStorage model

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
