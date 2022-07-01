b2343b3c5352ac7281d646e376a4ccd64a7b80d3
Carlos Alve <carlosalves369m@hotmail.com>
2022-07-01 12:05:56 -0300

    :lipstick: update Header style component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,18 +1,25 @@
 import styles from './styles.module.sass'
+import brand from '../../assets/brand.svg'
+import { MdSearch, MdShoppingBag } from 'react-icons/md'
 
 export function Header () {
     return (
         <header className={styles.contentHeader}>
             <div className={styles.brand}>
                 <img 
-                    src="./images/brand.png" 
-                    alt="Food Delivery" 
+                    src={brand} 
+                    alt="Food Delivery"
+                    width="200"
                 />
             </div>
             <div className={styles.menu}>
                 <ul>
-                    <li><a href="#">Restaurantes</a></li>
-                    <li><a href="#">Outros</a></li>
+                    <li><a href="#"><MdSearch/> Endereço</a></li>
+                    <li>
+                        <a href="#" className={styles.shoppingCart}><MdShoppingBag />
+                            <span className={styles.notification}>2</span>
+                        </a>
+                        </li>
                 </ul>
             </div>
         </header>
