9a87a9dbbd4af08a969765f3758486c8d63f69fb
Carlos Alve <carlosalves369m@hotmail.com>
2022-07-05 12:57:49 -0300

    :heavy_plus_sign: add axios dependency

--- frontend/src/pages/Restaurants/index.tsx
+++ frontend/src/pages/Restaurants/index.tsx
@@ -7,7 +7,31 @@ import android from '../../assets/button-android.png'
 import food from '../../assets/food.png'
 import { Footer } from '../../components/Footer'
 
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+import { toCurrency } from '../../services/toCurrency'
+import { modifyTitle } from '../../services/modifyTitle'
+
+
+type RestaurantProps = {
+    id: number,
+    name: string,
+    image_url: string,
+    delivery_tax: number
+}
+
 export function Restaurants () {
+
+    modifyTitle("Restaurantes")
+    
+    const [ restaurants, setRestaurants ] = useState<RestaurantProps[]>([])
+
+    useEffect(() => {
+        api
+            .get<RestaurantProps[]>('/api/restaurants')
+            .then((response) => setRestaurants(response.data))
+            .catch((err) => alert("Ops, ocorreu um erro: " + err))
+    }, [])
 
     return (
         <section className={styles.contentWrapper}>
@@ -52,101 +76,24 @@ export function Restaurants () {
             <div className={styles.restaurantsList}>
                 <h2>Lista de Restaurantes</h2>
                 <div className={styles.cards}>
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 1</h4>
-                                <p className={styles.restaurantType}>Comida Italiana</p>
-                                <p className={styles.restaurantDeliveryValue}>Entrega R$ 5</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 4.5</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 2</h4>
-                                <p className={styles.restaurantType}>Comida Mexicana</p>
-                                <p className={styles.restaurantDeliveryValue}>Entrega R$ 5</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 4.9</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 3</h4>
-                                <p className={styles.restaurantType}>Comida Vegana</p>
-                                <p className={styles.restaurantDeliveryValue}>Fechada</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 5.0</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 4</h4>
-                                <p className={styles.restaurantType}>Comida Francesa</p>
-                                <p className={styles.restaurantDeliveryValue}>Entrega R$ 5</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 4.7</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 1</h4>
-                                <p className={styles.restaurantType}>Comida Italiana</p>
-                                <p className={styles.restaurantDeliveryValue}>Entrega R$ 5</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 4.5</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 2</h4>
-                                <p className={styles.restaurantType}>Comida Mexicana</p>
-                                <p className={styles.restaurantDeliveryValue}>Entrega R$ 5</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 4.9</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 3</h4>
-                                <p className={styles.restaurantType}>Comida Vegana</p>
-                                <p className={styles.restaurantDeliveryValue}>Fechada</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 5.0</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
-                        </div>
-                    </a>
-
-                    <a href="#" >
-                        <div className={styles.card} >
-                            <div className={styles.contentCard}>
-                                <h4>Restaurante 4</h4>
-                                <p className={styles.restaurantType}>Comida Francesa</p>
-                                <p className={styles.restaurantDeliveryValue}>Entrega R$ 5</p>
-                                <p className={styles.restaurantReviews}><MdStar /> 4.7</p>
-                            </div>
-                            <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                        </div>
-                    </a>
+                    {restaurants.length <= 0 ? 
+                        <div>Nenhum restaurante encontrado</div>
+                        : restaurants.map(({ id, name, image_url, delivery_tax  }) => {
+                            return (
+                                <a href="#">
+                                    <div className={styles.card} >
+                                        <div className={styles.contentCard}>
+                                            <h4>{name}</h4>
+                                            <p className={styles.restaurantType}>Comida Italiana</p>
+                                            <p className={styles.restaurantDeliveryValue}>Entrega {toCurrency(delivery_tax)}</p>
+                                            <p className={styles.restaurantReviews}><MdStar /> 4.5</p>
+                                        </div>
+                                        <img src={image_url} alt={name} />
+                                    </div>
+                                </a>
+                            )
+                        })
+                    }
                 </div>
             </div>
 
