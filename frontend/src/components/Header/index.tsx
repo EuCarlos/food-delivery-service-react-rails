cfd99cd6febfdceabcbabc0efb37590ca9e0b710
Carlos Alve <carlosalves369m@hotmail.com>
2022-07-08 20:07:47 -0300

    :sparkles: add search bar

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,17 @@
 import styles from './styles.module.sass'
 import brand from '../../assets/brand.svg'
 import { MdSearch, MdShoppingBag } from 'react-icons/md'
+import { useState } from 'react'
 
 export function Header () {
+    const [searchInput, setSearchInput] = useState({ input_value: ''})
+
+    const redirectRoute = () => {
+        if (searchInput.input_value.length >= 0) {
+            location.replace(`/restaurantes?q=${searchInput.input_value}`)
+        }
+    }
+
     return (
         <header className={styles.contentHeader}>
             <div className={styles.brand}>
@@ -14,12 +23,23 @@ export function Header () {
             </div>
             <div className={styles.menu}>
                 <ul>
-                    <li><a href="#"><MdSearch/> Endereço</a></li>
+                    <li>
+                        <div className={styles.searchBar} >
+                            <input 
+                                type="search"
+                                placeholder='Pesquisar'
+                                onChange={(e) =>  setSearchInput({...searchInput, input_value: e.target.value})}
+                                value={ searchInput.input_value } 
+                                required 
+                            />
+                            <button onClick={() => redirectRoute()}><MdSearch size={24} /></button>
+                        </div>
+                    </li>
                     <li>
                         <a href="#" className={styles.shoppingCart}><MdShoppingBag />
                             <span className={styles.notification}>2</span>
                         </a>
-                        </li>
+                    </li>
                 </ul>
             </div>
         </header>
