a257a8b10f59d7930ed6367fa779dfafa1a1d3ff
Carlos Alve <carlosalves369m@hotmail.com>
2022-08-01 18:07:25 -0300

    :heavy_plus_sign: add Swagger dependecies

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   scope '/api', defaults: { format: :json } do
     resources :categories, only: [:index]
     resources :restaurants, only: [:index, :show]
